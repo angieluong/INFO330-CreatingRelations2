85f29ab995d0d8828d1f342500c9851307285f63
angieluong <angielena0212@gmail.com>
2023-04-29 09:59:01 -0700

    added comments

--- pokemon_import_1nf.sql
+++ pokemon_import_1nf.sql
@@ -1,3 +1,5 @@
+-- created a reference table using Justin's recursive query in order to
+-- normalize the database into first normal form
 create table firstnf AS
 WITH split(name, abilities, nextability) AS (
     select name, '' as abilities, abilities||',' as nextability
@@ -13,18 +15,23 @@ select name, abilities
 from split
 where abilities !=''
 order by name;
-	
+
+-- creates a temp table with all data from the original data (imported_pokemon_data)
+-- and drops the abilities column to avoid duplication during the join
 create table first_temp as
 select * from imported_pokemon_data;
 
 alter table first_temp
 drop column abilities;
 
+-- creates the final 1NF pokemon data table using our recursively broken down table
+-- (firstnf) and temp table (first_temp)
 create table firstnf_pokemon_data AS
 select firstnf.*, first_temp.*
 from firstnf
 join first_temp on first_temp.name=firstnf.name;
 
+-- drops the temporary tables we do not need anymore, leaving the 1NF table
 drop table imported_pokemon_data;
 drop table firstnf;
 drop table first_temp;
--- pokemon_import_2nf.sql
+++ pokemon_import_2nf.sql
@@ -1,3 +1,6 @@
+-- creates a table using the name and abilities from our 1NF data
+-- this is one 2NF table because the data is no longer partially
+-- dependent
 create table secondnf_t1 as
 	select name, abilities
 	from firstnf_pokemon_data;
@@ -5,9 +8,12 @@ create table secondnf_t1 as
 alter table firstnf_pokemon_data
 	drop column abilities;
 
+-- creates a table using the distinct rows from the same data without
+-- including the abilities column
+-- this is our second 2NF table
 create table secondnf_t2 as
 	select distinct *
 	from firstnf_pokemon_data;
 
-
+-- drops unneccesary temp table
 drop table firstnf_pokemon_data;
